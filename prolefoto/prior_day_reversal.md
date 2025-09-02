248eec597014f3c71d5270a237fea7924eacf5ac
TheRealDxRed <ben.dumouchel@gmail.com>
2025-09-02 14:11:59 -0400

    fixed manual OCO problems and ran some tests
    i'm really starting to hate backtrader, but i don't have the
    time/patience to implement my own backtesting platform right now

--- prolefoto/prior_day_reversal.md
+++ prolefoto/prior_day_reversal.md
@@ -149,3 +149,186 @@ else:                                               # backtrader uses positive a
 ```
 
 So basically, it's time to completely rewrite the trading logic. I get the feeling Copilot doesn't know how to read docs, which is ironic since the Backtrader source has been public on GH since 2015.
+
+### Post-hiatus clarity
+
+I'm coming back to this after a few days and... wow. I managed to make a buggy mess. Somehow, something as simple as writing data to `trades.csv` is failing. Let me get to work...
+
+It only took a couple minutes to fix the `trades.csv` writer bug and... wow. Somehow, it's exiting at 0 on every trade. Meaning there's some weird shit going on with my limits/stops. Let's add some more logging lines to see what's going on. I'm getting tired of pasting logs here, so I'll just explain what the problem is: the entry orders are being treated as market orders, not limits. I'm still not sure _why_, since I'm explicitly defining them as limits, but I'll do some investigating...
+
+Just found out my manual OCO was fucked, so I've gotta fix that first.
+
+Okay, order placement and execution issues have (largely) been solved. I'm sure I'll run into them again as this evolves, but we're placing and cancelling the right orders at the right times. Now, the issue is why they're getting filled at awkward prices. I'll start by reducing the time span I'm testing on. Running over the course of a year takes too long for debugging. Let's do a month.
+
+Here, have a log:
+
+```
+2025-09-02 12:32:40,363 - INFO - Strategy run completed
+2025-09-02 12:32:40,363 - INFO - Final Portfolio Value: 99947.58
+2025-09-02 12:32:40,363 - INFO - Sharpe Ratio: None
+2025-09-02 12:32:40,363 - INFO - Max Drawdown: 0.07%
+2025-09-02 12:32:40,363 - INFO - Trades executed: 16
+2025-09-02 12:32:40,363 - INFO - Number of winning trades: 7
+2025-09-02 12:32:40,363 - INFO - Number of losing trades: 9
+2025-09-02 12:32:40,363 - INFO - Win rate: 43.75%
+2025-09-02 12:32:40,363 - INFO - Net Profit: -52.42
+2025-09-02 12:32:40,363 - INFO - Profit Factor: -1.0
+2025-09-02 12:32:40,363 - INFO - Saving transactions to trades.csv
+2025-09-02 12:32:40,365 - INFO - Transactions saved to trades.csv
+```
+
+The above backtest ran through the month of August 2025 on XAU_USD. I wound up re-running it with the number of bars in market to give me an idea of each position's time-to-live.
+
+```
+2025-09-02 12:36:08,054 - INFO - Average number of bars in the market: 232.5
+2025-09-02 12:36:08,054 - INFO - Longest time in the market: 390 bars
+2025-09-02 12:36:08,054 - INFO - Shortest time in the market: 1 bars
+```
+
+Considering these are based on 1m candles, we're spending an average of (`232.5 / 60 = 3.875 = 3:52:30`) 3 hours, 52 minutes, and 30 seconds in each position. The shortest time was 1 minute and the longest was 6 and a half hours. Without even checking the price values, this seems to be doing what it's meant to. Now to see if we can optimize the parameters (remember, my SL and TP are both `0.5 * PDR`). I'll bring the backtest time back to 1 year now that things seem to be working well. I still don't know _how_ I was filling at weird prices, but it probably had to do with my manual OCO bug.
+
+## Optimization
+
+Let's go crazy and start with `SL = 0.1 * PDR` and `TP = 1.0 * PDR`. In theory, we should be 10x as likely to hit our SL before our TP. This _should_ result in a roughly 10% win rate. If it's any greater, there's a chance the entire strategy could be automated for long-term profits.
+
+Gold just finished, so let's check it out:
+
+```
+2025-09-02 12:45:31,361 - INFO - Strategy run completed
+2025-09-02 12:45:31,361 - INFO - Final Portfolio Value: 99998.89
+2025-09-02 12:45:31,361 - INFO - Sharpe Ratio: None
+2025-09-02 12:45:31,361 - INFO - Max Drawdown: 0.06%
+2025-09-02 12:45:31,361 - INFO - Trades executed: 194
+2025-09-02 12:45:31,361 - INFO - Number of winning trades: 64
+2025-09-02 12:45:31,361 - INFO - Number of losing trades: 130
+2025-09-02 12:45:31,361 - INFO - Win rate: 32.99%
+2025-09-02 12:45:31,361 - INFO - Average number of bars in the market: 52.78350515463917
+2025-09-02 12:45:31,361 - INFO - Longest time in the market: 390 bars
+2025-09-02 12:45:31,361 - INFO - Shortest time in the market: 1 bars
+2025-09-02 12:45:31,362 - INFO - Net Profit: -1.11
+2025-09-02 12:45:31,362 - INFO - Profit Factor: -1.0
+2025-09-02 12:45:31,362 - INFO - Saving transactions to trades.csv
+2025-09-02 12:45:31,365 - INFO - Transactions saved to trades.csv
+```
+
+Now that's the kind of WR I was looking for, but we're still losing overall. Let me take a look at `trades.csv`, as janky as its formatting is.
+
+Looks like stops are getting filled correctly, but limits are too far out to get filled before the close. Makes sense to me. Let's pull TP back a little. 90% PDR, maybe?
+
+```
+2025-09-02 13:04:18,514 - INFO - Starting Prior Day Reversal Strategy
+2025-09-02 13:04:18,514 - INFO - Connected to OANDA
+2025-09-02 13:04:18,523 - INFO - Data feeds added
+2025-09-02 13:04:18,523 - INFO - Starting Portfolio Value: 100000.00
+2025-09-02 13:04:18,523 - INFO - Running the strategy
+2025-09-02 13:04:18,615 - INFO - Prior Day Reversal Strategy initialized
+2025-09-02 13:04:18,615 - INFO - Stop Loss Percentage: 10.0%
+2025-09-02 13:04:18,615 - INFO - Profit Target Percentage: 90.0%
+2025-09-02 13:05:39,447 - INFO - Strategy run completed
+2025-09-02 13:05:39,447 - INFO - Final Portfolio Value: 100002.89
+2025-09-02 13:05:39,447 - INFO - Sharpe Ratio: None
+2025-09-02 13:05:39,447 - INFO - Max Drawdown: 0.06%
+2025-09-02 13:05:39,447 - INFO - Trades executed: 194
+2025-09-02 13:05:39,447 - INFO - Number of winning trades: 64
+2025-09-02 13:05:39,447 - INFO - Number of losing trades: 130
+2025-09-02 13:05:39,447 - INFO - Win rate: 32.99%
+2025-09-02 13:05:39,447 - INFO - Average number of bars in the market: 51.371134020618555
+2025-09-02 13:05:39,447 - INFO - Longest time in the market: 390 bars
+2025-09-02 13:05:39,447 - INFO - Shortest time in the market: 1 bars
+2025-09-02 13:05:39,447 - INFO - Net Profit: 2.89
+2025-09-02 13:05:39,448 - INFO - Profit Factor: N/A
+2025-09-02 13:05:39,448 - INFO - Saving transactions to trades.csv
+2025-09-02 13:05:39,451 - INFO - Transactions saved to trades.csv
+```
+
+Good news! We made $2.89 over the course of 272 trading sessions! We're rich!
+
+Honestly, though, I think this is PoC enough to increase the risk a little. How about 1% account balance per trade? After a brief intermission setting up the right leverage and margin parameters (per OANDA's website), we're off to run another test.
+
+```
+2025-09-02 13:14:34,981 - INFO - Starting Prior Day Reversal Strategy
+2025-09-02 13:14:34,981 - INFO - Connected to OANDA
+2025-09-02 13:14:34,991 - INFO - Data feeds added
+2025-09-02 13:14:34,991 - INFO - Starting Portfolio Value: 100000.00
+2025-09-02 13:14:34,991 - INFO - Running the strategy
+2025-09-02 13:14:35,221 - INFO - Prior Day Reversal Strategy initialized
+2025-09-02 13:14:35,222 - INFO - Stop Loss Percentage: 10.0%
+2025-09-02 13:14:35,222 - INFO - Profit Target Percentage: 90.0%
+2025-09-02 13:16:04,266 - INFO - Strategy run completed
+2025-09-02 13:16:04,266 - INFO - Final Portfolio Value: 94389.31
+2025-09-02 13:16:04,266 - INFO - Sharpe Ratio: None
+2025-09-02 13:16:04,266 - INFO - Max Drawdown: 24.46%
+2025-09-02 13:16:04,266 - INFO - Trades executed: 194
+2025-09-02 13:16:04,266 - INFO - Number of winning trades: 64
+2025-09-02 13:16:04,266 - INFO - Number of losing trades: 130
+2025-09-02 13:16:04,266 - INFO - Win rate: 32.99%
+2025-09-02 13:16:04,266 - INFO - Average number of bars in the market: 51.371134020618555
+2025-09-02 13:16:04,266 - INFO - Longest time in the market: 390 bars
+2025-09-02 13:16:04,266 - INFO - Shortest time in the market: 1 bars
+2025-09-02 13:16:04,266 - INFO - Net Profit: -5610.69
+2025-09-02 13:16:04,266 - INFO - Profit Factor: -1.0
+2025-09-02 13:16:04,266 - INFO - Saving transactions to trades.csv
+2025-09-02 13:16:04,270 - INFO - Transactions saved to trades.csv
+```
+
+Ouch. I guess our 32% WR isn't quite enough, likely because we aren't hitting our TP, just closing at 16:00. Time to bring the TP down another notch. Let's try 50% to see if we could _ever_ win this way.
+
+```
+2025-09-02 13:19:18,010 - INFO - Starting Prior Day Reversal Strategy
+2025-09-02 13:19:18,010 - INFO - Connected to OANDA
+2025-09-02 13:19:18,019 - INFO - Data feeds added
+2025-09-02 13:19:18,019 - INFO - Starting Portfolio Value: 100000.00
+2025-09-02 13:19:18,019 - INFO - Running the strategy
+2025-09-02 13:19:18,145 - INFO - Prior Day Reversal Strategy initialized
+2025-09-02 13:19:18,146 - INFO - Stop Loss Percentage: 10.0%
+2025-09-02 13:19:18,146 - INFO - Profit Target Percentage: 50.0%
+2025-09-02 13:20:41,179 - INFO - Strategy run completed
+2025-09-02 13:20:41,179 - INFO - Final Portfolio Value: 99681.10
+2025-09-02 13:20:41,179 - INFO - Sharpe Ratio: None
+2025-09-02 13:20:41,179 - INFO - Max Drawdown: 17.75%
+2025-09-02 13:20:41,179 - INFO - Trades executed: 194
+2025-09-02 13:20:41,179 - INFO - Number of winning trades: 66
+2025-09-02 13:20:41,179 - INFO - Number of losing trades: 128
+2025-09-02 13:20:41,179 - INFO - Win rate: 34.02%
+2025-09-02 13:20:41,179 - INFO - Average number of bars in the market: 42.93814432989691
+2025-09-02 13:20:41,179 - INFO - Longest time in the market: 390 bars
+2025-09-02 13:20:41,179 - INFO - Shortest time in the market: 1 bars
+2025-09-02 13:20:41,179 - INFO - Net Profit: -318.90
+2025-09-02 13:20:41,179 - INFO - Profit Factor: -1.0
+2025-09-02 13:20:41,179 - INFO - Saving transactions to trades.csv
+2025-09-02 13:20:41,183 - INFO - Transactions saved to trades.csv
+```
+
+That's... better? I still don't like it. Part of me wants to blame backtrader, but the rest of me honestly thinks this strategy is better off discretionary and not automated. I'll try 30% next, though. That _could_ get us somewhere.
+
+```
+2025-09-02 13:23:22,337 - INFO - Starting Prior Day Reversal Strategy
+2025-09-02 13:23:22,337 - INFO - Connected to OANDA
+2025-09-02 13:23:22,346 - INFO - Data feeds added
+2025-09-02 13:23:22,346 - INFO - Starting Portfolio Value: 100000.00
+2025-09-02 13:23:22,346 - INFO - Running the strategy
+2025-09-02 13:23:22,479 - INFO - Prior Day Reversal Strategy initialized
+2025-09-02 13:23:22,479 - INFO - Stop Loss Percentage: 10.0%
+2025-09-02 13:23:22,479 - INFO - Profit Target Percentage: 30.0%
+2025-09-02 13:24:42,495 - INFO - Strategy run completed
+2025-09-02 13:24:42,495 - INFO - Final Portfolio Value: 94261.26
+2025-09-02 13:24:42,495 - INFO - Sharpe Ratio: None
+2025-09-02 13:24:42,495 - INFO - Max Drawdown: 19.70%
+2025-09-02 13:24:42,495 - INFO - Trades executed: 194
+2025-09-02 13:24:42,495 - INFO - Number of winning trades: 67
+2025-09-02 13:24:42,495 - INFO - Number of losing trades: 127
+2025-09-02 13:24:42,495 - INFO - Win rate: 34.54%
+2025-09-02 13:24:42,495 - INFO - Average number of bars in the market: 28.577319587628867
+2025-09-02 13:24:42,495 - INFO - Longest time in the market: 390 bars
+2025-09-02 13:24:42,495 - INFO - Shortest time in the market: 1 bars
+2025-09-02 13:24:42,495 - INFO - Net Profit: -5738.74
+2025-09-02 13:24:42,495 - INFO - Profit Factor: -1.0
+2025-09-02 13:24:42,496 - INFO - Saving transactions to trades.csv
+2025-09-02 13:24:42,499 - INFO - Transactions saved to trades.csv
+```
+
+Unsurprisingly, that did _much_ worse than 50%. You can sort of prove this mathematically based on our win rate:
+
+> Since WR is approximately 1/3 (33.33%), that means we would need to win more than 3x what we would lose. Currently, we barely scratch past 33% WR, but we only win 3x (30% / 10%) our risk when we do. This makes the strategy essentially breakeven at these parameters.
+
+How about we try a smaller SL, then? I'll try 5% SL and 50% TP. This will make the minimum required WR roughly 10%. Still no guarantees this will work, though, since exiting at the close makes every trade nondeterministic.
--- prolefoto/prior_day_reversal.py
+++ prolefoto/prior_day_reversal.py
@@ -4,6 +4,7 @@ import os
 
 import backtrader as bt
 import btoandav20 as btv20
+import pandas as pd
 import pytz
 from backtrader import TimeFrame
 
@@ -11,11 +12,6 @@ from backtrader import TimeFrame
 # >
 # > 1. mark out the previous day high and low
 # > 2. enter short if price taps PDH, enter long if price tals PDL.
-#
-# We're going to calculate the previous day high and low using daily bars.
-# Then we'll use a lower time frame to trade the reversals.
-# The stop loss will be 1/2 PDH-PDL away from the entry price.
-# Same thing for the profit target. 1R as they say.
 
 StoreCls = btv20.stores.OandaV20Store
 DataCls = btv20.feeds.OandaV20Data
@@ -26,12 +22,20 @@ def main():
     cerebro = bt.Cerebro()
 
     # Add a strategy
-    cerebro.addstrategy(PriorDayReversal, stop_loss_perc=0.5, profit_target_perc=0.5)
+    cerebro.addstrategy(PriorDayReversal, stop_loss_perc=0.1, profit_target_perc=0.5)
     cerebro.broker.setcash(100000.0)
-    cerebro.broker.setcommission(commission=0.0)
-    cerebro.addsizer(bt.sizers.FixedSize, stake=1)
+    cerebro.broker.setcommission(
+        commission=0.0,
+        leverage=20.0,
+        margin=0.5,
+        mult=1.0,
+        stocklike=False,
+        interest=0.0,
+    )
     cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name="sharpe")
     cerebro.addanalyzer(bt.analyzers.DrawDown, _name="drawdown")
+    cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name="tradeanalyzer")
+    cerebro.addanalyzer(bt.analyzers.Transactions, _name="transactions")
 
     # Data feed
     storeargs = dict(
@@ -49,8 +53,8 @@ def main():
     data0kwargs = dict(
         timeframe=TimeFrame.Minutes,
         compression=1,
-        fromdate=bt.datetime.datetime(2020, 1, 1),
-        todate=bt.datetime.datetime(2025, 1, 1),
+        fromdate=bt.datetime.datetime(2024, 1, 1),
+        todate=bt.datetime.datetime(2024, 12, 31),
         tz=pytz.timezone("US/Eastern"),
         backfill_start=True,
         backfill=True,
@@ -65,8 +69,8 @@ def main():
     data1kwargs = dict(
         timeframe=TimeFrame.Days,
         compression=1,
-        fromdate=bt.datetime.datetime(2019, 12, 1),
-        todate=bt.datetime.datetime(2025, 1, 1),
+        fromdate=bt.datetime.datetime(2023, 12, 31),
+        todate=bt.datetime.datetime(2024, 12, 31),
         tz=pytz.timezone("US/Eastern"),
         backfill_start=True,
         backfill=True,
@@ -93,53 +97,77 @@ def main():
 
     sharpe = strat.analyzers.sharpe.get_analysis()
     drawdown = strat.analyzers.drawdown.get_analysis()
+    tradeanalyzer = strat.analyzers.tradeanalyzer.get_analysis()
+    transactions = strat.analyzers.transactions.get_analysis()
 
-    winners = sum(1 for t in strat.trades if t["pnl"] > 0)
-    losers = sum(1 for t in strat.trades if t["pnl"] <= 0)
+    logging.debug(f"Sharpe Ratio: {type(sharpe)}")
+    logging.debug(f"Drawdown: {type(drawdown)}")
+    logging.debug(f"Trade Analyzer: {type(tradeanalyzer)}")
+    logging.debug(f"Transactions: {type(transactions)}")
+
+    winners = tradeanalyzer.won.total if tradeanalyzer.won.total else 0
+    losers = tradeanalyzer.lost.total if tradeanalyzer.lost.total else 0
 
     logging.info(f"Sharpe Ratio: {sharpe.get('sharperatio', 'N/A')}")
 
     logging.info(f"Max Drawdown: {drawdown.max.drawdown:.2f}%")
 
-    logging.info(f"Trades executed: {len(strat.trades)}")
+    logging.info(
+        f"Trades executed: {tradeanalyzer.total.closed if tradeanalyzer.total.closed else 0}"
+    )
     logging.info(f"Number of winning trades: {winners}")
     logging.info(f"Number of losing trades: {losers}")
     logging.info(
-        f"Win rate: {winners / len(strat.trades) * 100:.2f}%"
-        if strat.trades
-        else "Win rate: N/A"
+        f"Win rate: { (winners / (winners + losers) * 100) if (winners + losers) > 0 else 0:.2f}%"
     )
 
-    # Write trades to CSV
-    with open("trades.csv", mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            [
-                "Datetime",
-                "Symbol",
-                "Size",
-                "Price",
-                "PnL",
-                "TradeID",
-            ]
-        )
-        for trade in strat.trades:
-            writer.writerow(
-                [
-                    trade["datetime"],
-                    trade["symbol"],
-                    trade["size"],
-                    trade["price"],
-                    trade["pnl"],
-                    trade["tradeid"],
-                ]
+    logging.info(
+        f"Average number of bars in the market: {tradeanalyzer.len.average if tradeanalyzer.len.average else 0}"
+    )
+    logging.info(
+        f"Longest time in the market: {tradeanalyzer.len.max if tradeanalyzer.len.max else 0} bars"
+    )
+    logging.info(
+        f"Shortest time in the market: {tradeanalyzer.len.min if tradeanalyzer.len.min else 0} bars"
+    )
+
+    logging.info(
+        f"Net Profit: {tradeanalyzer.pnl.net.total if tradeanalyzer.pnl.net.total else 0:.2f}"
+    )
+    logging.info(
+        f"Profit Factor: {tradeanalyzer.pnl.net.total / abs(tradeanalyzer.pnl.net.total) if tradeanalyzer.pnl.net.total and tradeanalyzer.pnl.net.total < 0 else 'N/A'}"
+    )
+
+    # save transactions to csv
+    logging.info("Saving transactions to trades.csv")
+
+    # format the DF correctly
+    rows = []
+    for dt, row in transactions.items():
+        for trans in row:
+            rows.append(
+                {
+                    "date": dt,
+                    "amount": trans[0],
+                    "price": trans[1],
+                    # "sid": trans[2],
+                    "symbol": trans[3],
+                    "value": trans[4],
+                }
             )
+
+    df = pd.DataFrame(rows)
+    df.to_csv("trades.csv")
+    logging.info("Transactions saved to trades.csv")
+
+    cerebro.plot()
 
 
 class PriorDayReversal(bt.Strategy):
     params = dict(
         stop_loss_perc=0.5,  # stop loss as a percentage of PDH-PDL range
         profit_target_perc=0.5,  # profit target as a percentage of PDH-PDL range
+        risk_per_trade=0.01,  # risk per trade as a percentage of account equity
     )
 
     def __init__(self):
@@ -148,11 +176,13 @@ class PriorDayReversal(bt.Strategy):
         self.datalow = self.datas[1].low
         self.pdh = None
         self.pdl = None
-        self.trades = []
-        self.order = None
-        self.stop_price = None
-        self.target_price = None
         self.last_date = None
+        self.long_orders = None
+        self.short_orders = None
+
+        logging.info("Prior Day Reversal Strategy initialized")
+        logging.info(f"Stop Loss Percentage: {self.p.stop_loss_perc * 100}%")
+        logging.info(f"Profit Target Percentage: {self.p.profit_target_perc * 100}%")
 
     def next(self):
         dt = self.datas[0].datetime.date(0)
@@ -165,53 +195,104 @@ class PriorDayReversal(bt.Strategy):
         if self.pdh is None or self.pdl is None:
             return
 
-        if not self.position:
-            if self.dataclose[0] >= self.pdh:
-                size = 1
-                self.order = self.sell(size=size)
-                range_size = self.pdh - self.pdl
-                self.stop_price = self.dataclose[0] + (
-                    range_size * self.p.stop_loss_perc
-                )
-                self.target_price = self.dataclose[0] - (
-                    range_size * self.p.profit_target_perc
-                )
-            elif self.dataclose[0] <= self.pdl:
-                size = 1
-                self.order = self.buy(size=size)
-                range_size = self.pdh - self.pdl
-                self.stop_price = self.dataclose[0] - (
-                    range_size * self.p.stop_loss_perc
-                )
-                self.target_price = self.dataclose[0] + (
-                    range_size * self.p.profit_target_perc
-                )
-        else:
-            if self.position.size > 0:
-                if self.dataclose[0] <= self.target_price:
-                    self.close()
-                elif self.dataclose[0] >= self.stop_price:
-                    self.close()
-            elif self.position.size < 0:
-                if self.dataclose[0] >= self.target_price:
-                    self.close()
-                elif self.dataclose[0] <= self.stop_price:
-                    self.close()
+        # execute only at NY open (9:30 AM Eastern)
+        if self.datas[0].datetime.time(0) == bt.datetime.time(9, 30):
+            pdr = self.pdh - self.pdl
 
-    def notify_trade(self, trade):
-        if trade.isclosed:
-            pnl = trade.pnl
-            commission = trade.commission
-            trade_info = {
-                "datetime": self.datas[0].datetime.datetime(0),
-                "symbol": self.datas[0]._name,
-                "size": trade.size,
-                "price": trade.price,
-                "pnl": pnl,
-                "commission": commission,
-                "tradeid": trade.ref,
-            }
-            self.trades.append(trade_info)
+            long_entry_price = self.pdl
+            long_stop_price = self.pdl - pdr * self.p.stop_loss_perc
+            long_limit_price = self.pdl + pdr * self.p.profit_target_perc
+
+            short_entry_price = self.pdh
+            short_stop_price = self.pdh + pdr * self.p.stop_loss_perc
+            short_limit_price = self.pdh - pdr * self.p.profit_target_perc
+
+            lot_size = int(
+                (self.broker.getvalue() * self.p.risk_per_trade)
+                / (pdr * self.p.stop_loss_perc)
+            )
+
+            logging.debug(f"Lot size calculated: {lot_size}")
+
+            logging.debug(
+                f"{dt} - Long Entry: {long_entry_price:.2f}, Stop: {long_stop_price:.2f}, Limit: {long_limit_price:.2f}"
+            )
+            logging.debug(
+                f"{dt} - Short Entry: {short_entry_price:.2f}, Stop: {short_stop_price:.2f}, Limit: {short_limit_price:.2f}"
+            )
+
+            self.long_orders = self.buy_bracket(
+                size=lot_size,
+                data=self.datas[0],
+                price=long_entry_price,
+                exectype=bt.Order.Limit,
+                stopprice=long_stop_price,
+                limitprice=long_limit_price,
+                stopexec=bt.Order.Stop,
+                limitexec=bt.Order.Limit,
+                valid=bt.Order.DAY,
+            )
+
+            for o in self.long_orders:
+                logging.debug(
+                    f"Long order created: {o.created.size} @ {o.created.price} ({o.ref})"
+                )
+
+            self.short_orders = self.sell_bracket(
+                size=lot_size,
+                data=self.datas[0],
+                price=short_entry_price,
+                exectype=bt.Order.Limit,
+                stopprice=short_stop_price,
+                limitprice=short_limit_price,
+                stopexec=bt.Order.Stop,
+                limitexec=bt.Order.Limit,
+                valid=bt.Order.DAY,
+            )
+
+            for o in self.short_orders:
+                logging.debug(
+                    f"Short order created: {o.created.size} @ {o.created.price} ({o.ref})"
+                )
+
+        # cancel unfilled orders at the end of the day (4:00 PM Eastern) and flatten
+        if self.datas[0].datetime.time(0) == bt.datetime.time(16, 0):
+            if self.long_orders:
+                for order in self.long_orders:
+                    if order.status in [bt.Order.Submitted, bt.Order.Accepted]:
+                        self.cancel(order)
+                self.long_orders = None
+            if self.short_orders:
+                for order in self.short_orders:
+                    if order.status in [bt.Order.Submitted, bt.Order.Accepted]:
+                        self.cancel(order)
+                self.short_orders = None
+
+            if self.position != 0:
+                self.close()
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            # cancel short entry if long entry is filled and vice versa
+            # i hate manual OCO like this but backtrader's OCO handling is fucked
+            if order.isbuy() and self.short_orders and self.position:
+                logging.debug(f"Long entry filled, cancelling short entry: {order.ref}")
+                for o in self.short_orders:
+                    self.cancel(o)
+                self.short_orders = None
+            elif order.issell() and self.long_orders and self.position:
+                logging.debug(f"Short entry filled, cancelling long entry: {order.ref}")
+                for o in self.long_orders:
+                    self.cancel(o)
+                self.long_orders = None
+
+        elif order.status in [order.Margin]:
+            logging.warning(f"Margin issue with order {order.ref}")
+        elif order.status in [order.Rejected]:
+            logging.warning(f"Order Rejected: {order.ref}")
 
 
 if __name__ == "__main__":
